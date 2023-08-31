6f83c32278a5fb8e9f463632ebe644bd1739b434
Amanda <acfreeman1022@gmail.com>
2023-08-31 10:41:25 -0500

    added 2nd message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
+System.out.println("Melina is the best!!!");
 		System.out.println("Amanda is the best partner!!");
 	}
 }
