c5ae6b9403050b8f743243ea970e6c83a44b92f5
melinagarren <melinagarren@localhost>
2023-08-31 10:34:22 -0500

    new message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Amanda is the best partner");
+		System.out.println("Amanda is the best partner!");
 	}
 }
